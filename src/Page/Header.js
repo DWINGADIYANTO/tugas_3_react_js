399ca0b984a79097c037ea104708dc7d7bc1e6a2
Dwi Ngadiyanto <dwingadiyanto879@gmail.com>
2023-02-11 16:39:43 +0700

    Tugas 3 React.js
    Tugas ini kami kumpulkan untuk Niomic

---
+++ src/Page/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h2 align="center">Home | Product | Kontak | Tentang Kami</h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
